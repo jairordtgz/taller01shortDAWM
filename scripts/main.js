6e4085109ce6cf977762ebbc29d1ee714ef7286c
Jairo Rodriguez <jairodri.a05@gmail.com>
2025-11-10 19:53:09 +0000

    cargar taller

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 // Plantilla para cada canción:
+"use strict";
+
+
+
 let card_song = `<article class="song-card">
     <div class="cover">
         <img src="https://i.ytimg.com/vi/V9PVRfjEBTI/maxresdefault.jpg"
@@ -24,3 +28,44 @@ let formatter = new Intl.NumberFormat('en-US');
 
 // let views_withformat= formatter.format(views_witoutformat); 
 // Valor de views_withformat: "2,536,628"
+
+let renderSongs = async () => {
+    try {
+        const response = await fetch("https://raw.githubusercontent.com/DATA-DAWM/Datos/refs/heads/main/Youtube/only_songs.json");
+
+        let container = document.getElementById("songs-grid");
+
+        let canciones = response.slice(0, 11);
+        let formatter = new Intl.NumberFormat('en-US'); 
+        canciones.forEach(cancion => {
+            
+            let mostrarCancion = `
+           <article class="song-card">
+    <div class="cover">
+        <img src="${cancion.thumbnail}"
+            alt="Portada: ${cancion.title}">
+        <span class="badge">${cancion.duration_string}/span>
+    </div>
+    <div class="content">
+        <h2 class="title">${cancion.title}</h2>
+        <div class="meta">${formatter.format(cancion.channel_follower_count)}</div>
+        <div class="footer">
+            <span class="channel">Canal: <a href="${cancion.channel_url}"
+                    target="_blank" rel="noopener noreferrer">${cancion.channel}</a></span>
+        </div>
+    </div>
+</article>`
+            container.innerHTML += mostrarCancion; 
+        })
+
+        
+
+    } catch (error) {
+        alert(`No se pudo cargar las categorias ${error.message}`);
+
+    }
+}
+
+(() => {
+    renderSongs(); 
+})();
